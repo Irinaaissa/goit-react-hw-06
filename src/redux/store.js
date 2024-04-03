7055c1e6f3959d6881dd4b352d25322466891de4
Irinaaissa <irinaalisa7506@gmail.com>
2024-04-03 11:39:52 +0300

    1

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
-import storage from 'redux-persist/lib/storage';
+import storage from 'redux-persist/lib/storage'; 
 import { persistStore, persistReducer } from 'redux-persist';
 
 const persistConfig = {
